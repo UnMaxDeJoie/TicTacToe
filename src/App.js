b59fae950eaef50228b0084d9b6d0c448e59a200
MaximeAitAdda <maxime.AIT-ADDA@habitat.fr>
2023-11-14 15:36:32 +0100

    start learn react

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-export default function Square() {
-  return (
-      <>
-        <div className="board-row">
-          <button className="square">1</button>
-          <button className="square">2</button>
-          <button className="square">3</button>
-        </div>
-        <div className="board-row">
-          <button className="square">4</button>
-          <button className="square">5</button>
-          <button className="square">6</button>
-        </div>
-        <div className="board-row">
-          <button className="square">7</button>
-          <button className="square">8</button>
-          <button className="square">9</button>
-        </div>
-      </>
-  );
+import { useState } from 'react';
+
+export default function Board() {
+    return (
+        <>
+            <div className="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+            <div className="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+            <div className="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+        </>
+    );
+}
+
+function Square() {
+    const [value, setValue] = useState(null);
+
+    function handleClick() {
+        alert({ value }+'clicked!');
+    }
+
+    return (
+        <button
+            className="square"
+            onClick={handleClick}
+        >
+            {value}
+        </button>
+    );
 }
